fff3808b700a21aa49124806148b8c7ed1d3307f
Moses Saffa Gbondo <gbondos@berea.edu>
2017-12-23 01:50:41 -0500

    Update Sum of Two Integers.py

--- Sum of Two Integers.py
+++ Sum of Two Integers.py
@@ -1,10 +1,8 @@
-Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
-
-Example:
-Given a = 1 and b = 2, return 3.
-
-Credits:
-Special thanks to @fujiaozhu for adding this problem and creating all test cases.
+# Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+# Example:
+# Given a = 1 and b = 2, return 3.
+# Credits:
+# Special thanks to @fujiaozhu for adding this problem and creating all test cases.
 
 class Solution:
     def getSum(self, a, b):
