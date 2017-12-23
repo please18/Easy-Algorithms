e516379f910914bf1aa468899b2c5701b410639b
Moses Saffa Gbondo <gbondos@berea.edu>
2017-12-23 01:40:37 -0500

    Update Reverse String.py

--- Reverse String.py
+++ Reverse String.py
@@ -1,6 +1,6 @@
-Write a function that takes a string as input and returns the string reversed.
-Example:
-Given s = "hello", return "olleh".
+# Write a function that takes a string as input and returns the string reversed.
+# Example:
+# Given s = "hello", return "olleh".
 
 class Solution:
     def reverseString(self, s):
