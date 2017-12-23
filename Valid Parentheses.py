d02bc3601298c6f9db8c33c48363e69635aa20a9
Moses Saffa Gbondo <gbondos@berea.edu>
2017-12-22 20:38:20 -0500

    Create Valid Parentheses.py

---
+++ Valid Parentheses.py
@@ -0,0 +1,20 @@
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+class Solution:
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        n = len(s)
+        while (n != 0 and n%2 == 0):
+            s = s.replace("()", "")
+            s = s.replace("{}", "")
+            s = s.replace("[]",  "")
+            if n > len(s):
+                n = len(s)
+            else:
+                n = 0
+        return s == ""
