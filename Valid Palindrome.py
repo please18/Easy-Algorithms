511505491e066221489ee1f577c1869a9777f397
Moses Saffa Gbondo <gbondos@berea.edu>
2017-12-23 01:11:18 -0500

    Update Valid Palindrome.py

--- Valid Palindrome.py
+++ Valid Palindrome.py
@@ -1,13 +1,13 @@
-Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+# Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
 
-For example,
-"A man, a plan, a canal: Panama" is a palindrome.
-"race a car" is not a palindrome.
+# For example,
+# "A man, a plan, a canal: Panama" is a palindrome.
+# "race a car" is not a palindrome.
 
-Note:
-Have you consider that the string might be empty? This is a good question to ask during an interview.
+# Note:
+# Have you consider that the string might be empty? This is a good question to ask during an interview.
 
-For the purpose of this problem, we define empty string as valid palindrome.
+# For the purpose of this problem, we define empty string as valid palindrome.
 
 class Solution:
     def isPalindrome(self, s):
